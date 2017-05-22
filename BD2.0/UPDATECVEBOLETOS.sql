17474087528da5eb68caaa6cf8d400cda74166cd
Karen González <karenggv19@gmail.com>
2017-05-21 20:26:19 -0600

    Cambiecillos c:
    - Generador de claves de boleto
    - MUCHAS modificaciones
    - Tabla nueva -> descuentos ;)
    - etc, tengo sueño c:

---
+++ BD2.0/UPDATECVEBOLETOS.sql
@@ -0,0 +1,272 @@
+USE reservación_vuelos;
+GO
+
+UPDATE boleto SET claveBoleto = 'V9LITWEOVVZ7HRZ' 
+  WHERE claveBoleto = '1'
+
+UPDATE boleto SET claveBoleto = '824X7MNTXCCR4MS' 
+  WHERE claveBoleto = '2'
+
+UPDATE boleto SET claveBoleto = 'E10X3UTS8TDM5PY' 
+  WHERE claveBoleto = '3'
+
+UPDATE boleto SET claveBoleto = 'QHOTMGMHE8IPXFQ' 
+  WHERE claveBoleto = '4'
+
+UPDATE boleto SET claveBoleto = 'FJCDQ8GTXEOK6E3' 
+  WHERE claveBoleto = '5'
+
+UPDATE boleto SET claveBoleto = 'ARE9V5CL9L2TWZV' 
+  WHERE claveBoleto = '6'
+
+UPDATE boleto SET claveBoleto = 'E5MRO3J180KX0O7' 
+  WHERE claveBoleto = '7'
+
+UPDATE boleto SET claveBoleto = '5QDK80LYUUC7C2Y' 
+  WHERE claveBoleto = '8'
+
+UPDATE boleto SET claveBoleto = 'OU4CAT8PZ900FJL' 
+  WHERE claveBoleto = '9'
+
+UPDATE boleto SET claveBoleto = 'HL7CPLQL042H16L' 
+  WHERE claveBoleto = '10'
+
+UPDATE boleto SET claveBoleto = '6G9KLZ3YNN7BNKH' 
+  WHERE claveBoleto = '11'
+
+UPDATE boleto SET claveBoleto = 'AGOIN0AAMM8JDHH' 
+  WHERE claveBoleto = '12'
+
+UPDATE boleto SET claveBoleto = 'MGSODT057VOXTYS' 
+  WHERE claveBoleto = '13'
+
+UPDATE boleto SET claveBoleto = 'KDAT3W2TTU6GW7N' 
+  WHERE claveBoleto = '14'
+
+UPDATE boleto SET claveBoleto = 'QZQGB1S9HYKLQW8' 
+  WHERE claveBoleto = '15'
+
+UPDATE boleto SET claveBoleto = 'DQM4C5JUOVUX93H' 
+  WHERE claveBoleto = '16'
+
+UPDATE boleto SET claveBoleto = 'ND7UFYGNX01D24T' 
+  WHERE claveBoleto = '17'
+
+UPDATE boleto SET claveBoleto = 'KL6URP325SHFCUR' 
+  WHERE claveBoleto = '18'
+
+UPDATE boleto SET claveBoleto = 'ETGAKGN6RK7DZDK' 
+  WHERE claveBoleto = '19'
+
+UPDATE boleto SET claveBoleto = '1QZ4LI1OL1TLCC8' 
+  WHERE claveBoleto = '20'
+
+UPDATE boleto SET claveBoleto = 'TE3FWI72UN6POKU' 
+  WHERE claveBoleto = '21'
+
+UPDATE boleto SET claveBoleto = 'MEYU00PHJINE4I1' 
+  WHERE claveBoleto = '22'
+
+UPDATE boleto SET claveBoleto = '4L3S02JTRC3XFQO' 
+  WHERE claveBoleto = '23'
+
+UPDATE boleto SET claveBoleto = '3ME1POXRLHD6T7J' 
+  WHERE claveBoleto = '24'
+
+UPDATE boleto SET claveBoleto = 'DDU3TVV6T1CVHS7' 
+  WHERE claveBoleto = '25'
+
+UPDATE boleto SET claveBoleto = 'DK04PAAC2MH3RLX' 
+  WHERE claveBoleto = '26'
+
+UPDATE boleto SET claveBoleto = '60WH80DM72HQUAN' 
+  WHERE claveBoleto = '27'
+
+UPDATE boleto SET claveBoleto = '3P9OE3FYRIDA1QL' 
+  WHERE claveBoleto = '28'
+
+UPDATE boleto SET claveBoleto = '1EU65M9ATGA83CI' 
+  WHERE claveBoleto = '29'
+
+UPDATE boleto SET claveBoleto = 'UPCPT14DH30BE85' 
+  WHERE claveBoleto = '30'
+
+UPDATE boleto SET claveBoleto = 'ZL5VBD5YFA3PPHE' 
+  WHERE claveBoleto = '31'
+
+UPDATE boleto SET claveBoleto = '5MWJXX8L7T7BNCE' 
+  WHERE claveBoleto = '32'
+
+UPDATE boleto SET claveBoleto = 'L5BYRYCHAFZ16OZ' 
+  WHERE claveBoleto = '33'
+
+UPDATE boleto SET claveBoleto = 'MP5C8OO4KI198CD' 
+  WHERE claveBoleto = '34'
+
+UPDATE boleto SET claveBoleto = 'CRVAZ8N10P2EVE8' 
+  WHERE claveBoleto = '35'
+
+UPDATE boleto SET claveBoleto = '5B7IHSFKD06A1PZ' 
+  WHERE claveBoleto = '36'
+
+UPDATE boleto SET claveBoleto = 'YEF7CIG6Y08XEWG' 
+  WHERE claveBoleto = '37'
+
+UPDATE boleto SET claveBoleto = 'J20MNZC04V6GRJ3' 
+  WHERE claveBoleto = '38'
+
+UPDATE boleto SET claveBoleto = '8WLA1Y9YBZYFIRI' 
+  WHERE claveBoleto = '39'
+
+UPDATE boleto SET claveBoleto = '4BX7RDKOCAVOKLG' 
+  WHERE claveBoleto = '40'
+
+UPDATE boleto SET claveBoleto = 'CYW52MMD5SJDYWY' 
+  WHERE claveBoleto = '41'
+
+UPDATE boleto SET claveBoleto = 'U4IW14SU2ZYIIAF' 
+  WHERE claveBoleto = '42'
+
+UPDATE boleto SET claveBoleto = 'JZN4XASMZ5S72PI' 
+  WHERE claveBoleto = '43'
+
+UPDATE boleto SET claveBoleto = 'AV1OQ1FXULTE4GD' 
+  WHERE claveBoleto = '44'
+
+UPDATE boleto SET claveBoleto = 'EDD0IU038XBRS0I' 
+  WHERE claveBoleto = '45'
+
+UPDATE boleto SET claveBoleto = '70FUZCQ4UNDB2TE' 
+  WHERE claveBoleto = '46'
+
+UPDATE boleto SET claveBoleto = '5YGV1P4BGYJ220O' 
+  WHERE claveBoleto = '47'
+
+UPDATE boleto SET claveBoleto = 'JW6LO39KRBXITME' 
+  WHERE claveBoleto = '48'
+
+UPDATE boleto SET claveBoleto = 'S7K42NL793DY86P' 
+  WHERE claveBoleto = '49'
+
+UPDATE boleto SET claveBoleto = 'KXDFEREQEK6OYTD' 
+  WHERE claveBoleto = '50'
+
+UPDATE boleto SET claveBoleto = 'GLZBFLR78B7Z7SL' 
+  WHERE claveBoleto = '51'
+
+UPDATE boleto SET claveBoleto = 'FAOB1AENIXNQFU5' 
+  WHERE claveBoleto = '52'
+
+UPDATE boleto SET claveBoleto = '9XN5X2960E2ZFEG' 
+  WHERE claveBoleto = '53'
+
+UPDATE boleto SET claveBoleto = '1H83F69X2DB7QO2' 
+  WHERE claveBoleto = '54'
+
+UPDATE boleto SET claveBoleto = '4LNDCKLC4TGDX79' 
+  WHERE claveBoleto = '55'
+
+UPDATE boleto SET claveBoleto = 'L9JC5GSMP899V2V' 
+  WHERE claveBoleto = '56'
+
+UPDATE boleto SET claveBoleto = '4SS8JQLCB5PEEJ8' 
+  WHERE claveBoleto = '57'
+
+UPDATE boleto SET claveBoleto = '2MZ32BXOSVM98ED' 
+  WHERE claveBoleto = '58'
+
+UPDATE boleto SET claveBoleto = 'K0KIYXVZXS97HGQ' 
+  WHERE claveBoleto = '59'
+
+UPDATE boleto SET claveBoleto = 'BNCQFXK29UHE9M7' 
+  WHERE claveBoleto = '60'
+
+UPDATE boleto SET claveBoleto = 'QN78UZ35VLHWBK2' 
+  WHERE claveBoleto = '61'
+
+UPDATE boleto SET claveBoleto = 'EPHCU87R8PA8KYR' 
+  WHERE claveBoleto = '62'
+
+UPDATE boleto SET claveBoleto = 'TVN0BV9JV0HSRF8' 
+  WHERE claveBoleto = '63'
+
+UPDATE boleto SET claveBoleto = 'AIRHP16728T58SU' 
+  WHERE claveBoleto = '64'
+
+UPDATE boleto SET claveBoleto = '1TGE1WILJGEGVLM' 
+  WHERE claveBoleto = '65'
+
+UPDATE boleto SET claveBoleto = 'PPMAP2RDJZWYV2R' 
+  WHERE claveBoleto = '66'
+
+UPDATE boleto SET claveBoleto = '0B0B9YMNNM26IAK' 
+  WHERE claveBoleto = '67'
+
+UPDATE boleto SET claveBoleto = 'VKVPSDT8D1JQ7OE' 
+  WHERE claveBoleto = '68'
+
+UPDATE boleto SET claveBoleto = '8WS1QDPUMXDC8YW' 
+  WHERE claveBoleto = '69'
+
+UPDATE boleto SET claveBoleto = '5IC9ELCFJNGDWJZ' 
+  WHERE claveBoleto = '70'
+
+UPDATE boleto SET claveBoleto = 'IP8JOFNQTGF0J1O' 
+  WHERE claveBoleto = '71'
+
+UPDATE boleto SET claveBoleto = 'BVJR6BN8K25QJNK' 
+  WHERE claveBoleto = '72'
+
+UPDATE boleto SET claveBoleto = 'A3KWPYEZKQR1T28' 
+  WHERE claveBoleto = '73'
+
+UPDATE boleto SET claveBoleto = 'V3O4MV3FNY4QYQS' 
+  WHERE claveBoleto = '74'
+
+UPDATE boleto SET claveBoleto = 'ZEDJFMPWM0659AO' 
+  WHERE claveBoleto = '75'
+
+UPDATE boleto SET claveBoleto = 'J4FLHBCOAYHX78U' 
+  WHERE claveBoleto = '76'
+
+UPDATE boleto SET claveBoleto = 'HPJ11NYJXWFHBEQ' 
+  WHERE claveBoleto = '77'
+
+UPDATE boleto SET claveBoleto = 'Z4F8PALNIQWZCU8' 
+  WHERE claveBoleto = '78'
+
+UPDATE boleto SET claveBoleto = 'YSX1SRWHIBUUSYB' 
+  WHERE claveBoleto = '79'
+
+UPDATE boleto SET claveBoleto = '4BKU9NS8TU6CAKF' 
+  WHERE claveBoleto = '80'
+
+UPDATE boleto SET claveBoleto = 'XVZY8XTJV8DW674' 
+  WHERE claveBoleto = '81'
+
+UPDATE boleto SET claveBoleto = 'Q3YHWVPSVVFTEHW' 
+  WHERE claveBoleto = '82'
+
+UPDATE boleto SET claveBoleto = 'OV6DO9530NK1YV5' 
+  WHERE claveBoleto = '83'
+
+UPDATE boleto SET claveBoleto = '0WAYDCN3YUIDDD3' 
+  WHERE claveBoleto = '84'
+
+UPDATE boleto SET claveBoleto = 'DPC9TRZ3T5U983L' 
+  WHERE claveBoleto = '85'
+
+UPDATE boleto SET claveBoleto = '306Z4SSPDTB2ISG' 
+  WHERE claveBoleto = '86'
+
+UPDATE boleto SET claveBoleto = '51Z6J5ZZBOA3R3T' 
+  WHERE claveBoleto = '87'
+
+UPDATE boleto SET claveBoleto = 'CO9M0RGNJKU98PS' 
+  WHERE claveBoleto = '88'
+
+UPDATE boleto SET claveBoleto = 'ZRKN92KBTQEB4L4' 
+  WHERE claveBoleto = '89'
+
+UPDATE boleto SET claveBoleto = 'ZBWUQW262YZNQWY' 
+  WHERE claveBoleto = '90'
---
+++ BD2.0/clavesRandomSofisticadasDeBajo/clavesRandomSofisticadasDeBajo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+
+namespace clavesRandomSofisticadasDeBajo {
+	class Program {
+
+		 ArrayList arrayBoletos = new ArrayList();
+		 Random random = new Random();
+		 string claveBoleto;
+		 string script = "USE reservación_vuelos;\nGO\n";
+
+		static void Main(string[] args) {
+
+			Program p = new Program();
+			for (byte i = 1; i < 91; i++) {
+				do {
+					p.claveBoleto = p.RandomKey(15).Trim().ToUpper();
+				} while (p.existeBoleto(p.claveBoleto));
+
+				p.arrayBoletos.Add(p.claveBoleto);
+				p.script += string.Format("\nUPDATE boleto SET claveBoleto = '{0}' \n  WHERE claveBoleto = '{1}'\n",
+										p.claveBoleto,i);
+			}
+
+			Console.WriteLine(p.script);
+			Console.WriteLine("\n{0}",p.script);
+			System.IO.File.WriteAllText(@"UPDATECVEBOLETOS.sql",p.script);
+			Console.WriteLine("\n>> SCRIPT GENERADO CON ÉXITO <<");
+			Console.ReadLine();
+
+		}
+
+		private bool existeBoleto(string claveBoleto) {
+			foreach (string val in arrayBoletos)
+				if (val.Equals(claveBoleto))
+					return true;
+
+			return false;
+		}
+
+		private string RandomKey(int length) {
+			const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			return new string(Enumerable.Repeat(chars,length)
+			  .Select(s => s[random.Next(s.Length)]).ToArray());
+		}
+
+	}
+}
